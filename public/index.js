fd85c0ec79340e94d6e1af6b932662d43e2ae3d6
jwats287 <jwats287@users.noreply.github.com>
2018-03-13 21:37:45 -0400

    Clicking on a title displays info about the movie

--- public/index.js
+++ public/index.js
@@ -1,25 +1,41 @@
 $(document).ready(function() {
     console.log("Up and running");
+    let movieResults = [];
+    // Handle search form submit //
     $("#movie-search").on("click", function(e) {
         e.preventDefault();
         const movieName = $("#movie-name").val().trim();
-        const queryURL = "https://www.omdbapi.com/?s=" + movieName + "&plot=short&apikey=aaa8096b";
+        const queryURL = "https://www.omdbapi.com/?s=" + movieName + "&apikey=aaa8096b";
         $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
-            // console.log(response);
+            console.log(response);
+            movieResults = response.Search;
+            console.log(movieResults);
             if (response.Response === "True") {
-                response.Search.map((movie) => {
-                    console.log(movie.Title)
-                    $('#search-results').append(`<p>${movie.Title}</p>`)
+                movieResults.map((movie) => {
+                    $('#search-results').append(`<p id=${movie.imdbID}>${movie.Title}</p>`)
                 })
             }
             else {
                 console.log("No results found");
             }
-            
         });
     });
+    // Display Additional Information when title is clicked //
+    $('#search-results').on('click', 'p', function(e) {        
+        const movieID = e.target.id;
+        const queryURL = "https://www.omdbapi.com/?i=" + movieID + "&plot=short&apikey=aaa8096b";
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+            console.log(response);
+            $(`#${e.target.id}`).append(`<ul><li>Director: ${response.Director}</li><li>Year: ${response.Year}</li><li>IMDB Rating: ${response.imdbRating}</li><li>Plot: ${response.Plot}</li></ul><img src=${response.Poster}/>`);
+        });
+        
+    })
+
 
 });
